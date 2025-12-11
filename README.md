36678c75d3b23399344492bb3b401fc3d6a15cce
Markiyan <Markianvladyka1998@gmail.com>
2025-12-11 14:29:08 +0200

    commit lab4

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ ssh -i my-key.pem ubuntu@                        зайти в інстанс
 id adminuser                                     перевірити користувача adminuser
 id poweruser                                     перевірити користувача poweruser
 sudo cat /root/adminuser_password.txt            перевірити пароль для adminuser
-su - adminuser або ssh adminuser ubuntu@         увійти в adminuser
+su - adminuser                                   увійти в adminuser
 su - poweruser                                   увійти в poweruser
 ls -l /home/adminuser                            перевірити доступ poweruser до adminuser
 ```
--- data.sh
+++ data.sh
@@ -13,21 +13,16 @@ usermod -aG sudo adminuser
 echo "adminuser password: $PASSWORD" > /root/adminuser_password.txt
 chmod 600 /root/adminuser_password.txt
 
-sed -i 's/^#PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sed -i 's/^#KbdInteractiveAuthentication.*/KbdInteractiveAuthentication yes/' /etc/ssh/sshd_config
-sed -i 's/^PermitEmptyPasswords.*/PermitEmptyPasswords no/' /etc/ssh/sshd_config
-
 useradd -m -s /bin/bash poweruser
 passwd -d poweruser
 
 echo "poweruser ALL=(ALL) NOPASSWD: /usr/sbin/iptables" > /etc/sudoers.d/poweruser
 chmod 440 /etc/sudoers.d/poweruser
 
+usermod -aG adminuser poweruser
+
 chmod 750 /home/adminuser
-chgrp poweruser /home/adminuser
 
 ln -s /etc/mtab /home/poweruser/mtab-link
-
 
 systemctl restart sshd
